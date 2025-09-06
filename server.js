f0f616c860e0be3cfd061ff022d7e80d12fa5c0f
kreker36barabashka <tackabiog@hotmail.com>
2025-09-06 14:03:34 -0500

    Refactor MongoDB connection message for clarity.

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.json());
 
 // Kết nối đến MongoDB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/kien-truc-scalable', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Kết nối MongoDB thành công!'))
-  .catch(err => console.error('Kết nối MongoDB thất bại:', err));
+  .then(() => console.log('Kết nối thành công đến MongoDB!'))
+  .catch(err => console.error('Không thể kết nối đến MongoDB:', err));
 
 // Định nghĩa một dịch vụ mẫu
 app.get('/api/test', (req, res) => {
